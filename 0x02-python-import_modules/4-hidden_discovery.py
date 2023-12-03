ee754217e182ecbadeedd6d1a9b916a91025d1b1
Wambasi <wambasivin@gmail.com>
2023-12-03 01:25:58 -0800

    Update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-import dis
-with open('hidden_4.py', 'rb') as file:
-    bytecode = file.read()
-dis.dis(bytecode)
+
+if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
