458bfd054b2d75ae4a8d33f7d930c1c09bbdf92d
Wambasi <wambasivin@gmail.com>
2024-01-30 02:43:40 -0800

    Almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Module for Rectangle class.'''
-form models.base import Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -14,91 +14,93 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            '''Width of this recctangle.'''
-            return self.__width
+    @property
+    def width(self):
+        '''Width of this rectangle.'''
+        return self.__width
 
-        @width.setter
-        def height(self, value):
-            self.validate_integer("height", value, False)
-            self.__height = value
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
 
-        @property
-        def height(self):
-            '''Height of this rectangle.'''
-            return self.__height
+    @property
+    def height(self):
+        '''Height of this rectangle.'''
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            self.validate_integer("height", value, False)
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
 
-        @property
-        def x(self):
-            '''x of this rectangle.'''
-            return self.__x
+    @property
+    def x(self):
+        '''x of this rectangle.'''
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            self.validate_integer("x", value)
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
 
-        @property
-        def y(self, value):
-            '''y of this rectangle.'''
-            return self.__y
+    @property
+    def y(self):
+        '''y of this rectangle.'''
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            self.validate_integer("y", value)
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
 
-            def validate_integer(self, name, value, eq=True):
-                '''Method for validating the value.'''
-                if type(value) != int:
-                    raise TypeError("{} must be an integer".format(name))
-                if eq and value < 0:
-                    raise ValueError("{} must be > 0".format(name))
-                elif not eq and value <= 0:
-                    raise ValueError("{} must be > 0".format(name))
+    def validate_integer(self, name, value, eq=True):
+        '''Method for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
 
-        def area(self):
-            '''Computes area of this rectangle.'''
-            return self.width * self.height
+    def area(self):
+        '''Computes area of this rectangle.'''
+        return self.width * self.height
 
-        def display(self):
-            '''Prints string representation of this rectangle.'''
-            s = '\n' * self.y + \
-                    (' ' * self.x + '#' * self.width + '\n') * self.height
-            print(s, end='')
+    def display(self):
+        '''Prints string representation of this rectangle.'''
+        s = '\n' * self.y + \
+            (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s, end='')
 
-        def __str__(self):
-            '''Returns string info about this rectangle.'''
-            return '[{}] ({}) {}/{} - {}/{}'.\
-                    format(type(self).__name__, self.id, self.x, self.y, self.width, self.height)
+    def __str__(self):
+        '''Returns string info about this rectangle.'''
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
 
-        def __update(self, id=None, width=None, height=None, x=None, y=None):
-            '''Internal method that updates instance attributes via */**args.'''
-            if id is not None:
-                self.id = id
-            if width is not None:
-                self.width = width
-            if height is not None:
-                self.height = height
-            if x is not None:
-                self.x = x
-            if y is not None:
-                self.y = y
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''Internal method that updates instance attributes via */**args.'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
-        def update(self, *args, **kwargs):
-            '''Updates instance attributes via no-kkeyword & keyword args.'''
-            # print(args, kwargs)
-            if args:
-                self.__update(*args)
-            elif kwargs:
-                self.__update(**kwargs)
+    def update(self, *args, **kwargs):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        # print(args, kwargs)
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
-        def to_dictinary(self):
-            '''Returns dictionary representation of this class.'''
-            return {"id": self.id, "width": self.__width, "height": self.__height, "x": self.__x, "y": self.__y}
+    def to_dictionary(self):
+        '''Returns dictionary representation of this class.'''
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
