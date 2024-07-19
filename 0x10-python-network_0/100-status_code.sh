0e40f0ce2827f5b7ec98d17e9318829496b5f79e
Wambasi <wambasivin@gmail.com>
2024-07-19 02:01:48 -0700

    Added comments

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#sends a requst to a URL as an arguement and displays the status code only
 curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+#script that sendsa JSON POST request to a URL as the first arguement, displays body response
 curl -s -X POST -H "Content-Type: application/json" -d "@$2" "$1"
